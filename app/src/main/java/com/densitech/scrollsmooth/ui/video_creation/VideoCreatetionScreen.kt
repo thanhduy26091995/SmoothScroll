494ff3ebebf5777307597c8b62d4f3fc8844481e
DESKTOP-L482ENJ\Admin <buivuthanhduy@gmail.com>
2024-08-20 00:46:01 +0700

    Chore: Update UI

--- app/src/main/java/com/densitech/scrollsmooth/ui/video_creation/VideoCreatetionScreen.kt
+++ app/src/main/java/com/densitech/scrollsmooth/ui/video_creation/VideoCreatetionScreen.kt
@@ -1,12 +1,29 @@
 package com.densitech.scrollsmooth.ui.video_creation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,36 +33,51 @@ import androidx.compose.ui.unit.dp
 fun VideoTrimming(
     videoDuration: Long,
     onTrimChange: (Long, Long) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     var startTrim by remember { mutableLongStateOf(0L) }
     var endTrim by remember { mutableLongStateOf(videoDuration) }
     val handleWidth = 16.dp
-    val sliderWidth =
 
     // This will allow us to get the width of the timeline
     BoxWithConstraints(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
     ) {
         val timelineWidth = constraints.maxWidth.toFloat()
         val density = LocalDensity.current
         val handleWidthPx = with(density) { handleWidth.toPx() }
-// Calculate positions in pixels, then convert to Dp
+
         val startTrimOffsetDp = with(density) {
             ((startTrim / videoDuration.toFloat()) * timelineWidth - handleWidthPx / 2).toDp()
         }
         val endTrimOffsetDp = with(density) {
             ((endTrim / videoDuration.toFloat()) * timelineWidth - handleWidthPx / 2).toDp()
         }
-        // Timeline background
+
+        val sliderWidthDp = with(density) {
+            handleWidthPx.toDp()
+        }
+
         Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(4.dp)
-                .background(Color.Gray)
-                .padding(horizontal = handleWidth)
+                .offset(x = startTrimOffsetDp + sliderWidthDp)
+                .width(endTrimOffsetDp - startTrimOffsetDp - handleWidth)
+                .height(40.dp)
+                .drawBehind {
+                    drawRect(
+                        color = Color.Red,
+                        topLeft = Offset(0f, 0f),
+                        size = Size(size.width, 1.dp.toPx())
+                    )
+
+                    drawRect(
+                        color = Color.Red,
+                        topLeft = Offset(0f, size.height - 1.dp.toPx()),
+                        size = Size(size.width, 1.dp.toPx())
+                    )
+                }
         )
 
         // Start Handle
@@ -53,11 +85,18 @@ fun VideoTrimming(
             modifier = Modifier
                 .offset(x = startTrimOffsetDp)
                 .size(handleWidth, 40.dp)
-                .background(Color.Blue)
+                .border(
+                    1.dp,
+                    Color.Red,
+                    shape = RoundedCornerShape(topStart = 4.dp, bottomStart = 4.dp)
+                )
+                .clip(RoundedCornerShape(topStart = 4.dp, bottomStart = 4.dp))
+                .background(Color.Red)
                 .draggable(
                     orientation = Orientation.Horizontal,
                     state = rememberDraggableState { delta ->
-                        val newTrim = (startTrim + (delta / timelineWidth) * videoDuration).toLong()
+                        val newTrim =
+                            (startTrim + (delta / timelineWidth) * videoDuration).toLong()
                         startTrim = newTrim.coerceIn(0L, endTrim)
                         onTrimChange(startTrim, endTrim)
                     }
@@ -69,11 +108,18 @@ fun VideoTrimming(
             modifier = Modifier
                 .offset(x = endTrimOffsetDp)
                 .size(handleWidth, 40.dp)
+                .border(
+                    1.dp,
+                    Color.Red,
+                    shape = RoundedCornerShape(topEnd = 4.dp, bottomEnd = 4.dp)
+                )
+                .clip(RoundedCornerShape(topEnd = 4.dp, bottomEnd = 4.dp))
                 .background(Color.Red)
                 .draggable(
                     orientation = Orientation.Horizontal,
                     state = rememberDraggableState { delta ->
-                        val newTrim = (endTrim + (delta / timelineWidth) * videoDuration).toLong()
+                        val newTrim =
+                            (endTrim + (delta / timelineWidth) * videoDuration).toLong()
                         endTrim = newTrim.coerceIn(startTrim, videoDuration)
                         onTrimChange(startTrim, endTrim)
                     }
